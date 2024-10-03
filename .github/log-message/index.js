dd2bb901500ac02b3529a0a7b1af3824d5ea1af5
shankaramanchi <sairam.shankaramanchi@gmail.com>
2024-10-03 12:09:28 +0530

    modified actions files

--- .github/log-message/index.js
+++ .github/log-message/index.js
@@ -1,8 +1,9 @@
 const core = require('@actions/core');
 
 try {
+  // Get the message input defined in action.yml
   const message = core.getInput('message');
-  console.log(`Log message: ${message}`);
+  console.log(`Message: ${message}`);
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(`Action failed with error ${error}`);
 }
