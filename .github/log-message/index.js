09497300a8377dbfb637ecfc8725d8bf2ed54ab6
shankaramanchi <sairam.shankaramanchi@gmail.com>
2024-10-07 09:33:14 +0530

    deleted log message files

--- .github/log-message/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-
-try {
-  // Get the message input defined in action.yml
-  const message = core.getInput('message');
-  console.log(`Message: ${message}`);
-} catch (error) {
-  core.setFailed(`Action failed with error ${error}`);
-}
